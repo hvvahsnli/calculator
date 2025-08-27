5fbc93850cce4ef41628fb1725abacf364326cfe
hvvahsnli <hvvahsnliyeva@gmail.com>
2025-08-28 01:53:38 +0400

    Changes operators

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -19,14 +19,14 @@ function press(btn) {
   } else if (btn === 'DEL') {
     display.value = display.value.slice(0, -1) || '0'
   } else if (btn === '=') {
-  try {
-    let result = eval(display.value)
-    display.value = Math.round(result * 1e12) / 1e12
-    isResult = true
-  } catch {
-    display.value = "Error"
-    isResult = true
-  }
+    try {
+      let result = eval(display.value)
+      display.value = Math.round(result * 1e12) / 1e12
+      isResult = true
+    } catch {
+      display.value = "Error"
+      isResult = true
+    }
   } else if (btn === '%') {
     display.value = (parseFloat(display.value) / 100).toString()
     isResult = true
@@ -37,35 +37,36 @@ function press(btn) {
     display.value = Math.sqrt(parseFloat(display.value)).toString()
     isResult = true
   } else {
-    if (display.value === '0' || isResult) {
-      display.value = btn
+    if (isResult) {
+      if (['+', '-', '*', '/'].includes(btn)) {
+        display.value += btn
+      } else {
+        display.value = btn
+      }
       isResult = false
+    } else if (display.value === '0' && !isNaN(btn)) {
+      display.value = btn
     } else {
       display.value += btn
     }
   }
 }
-</script>
 
+</script>
 
 <template>
 
   <div class="bg-black p-6 rounded-2xl shadow-xl w-80">
-    <!-- Display -->
     <div class="mb-4 bg-pink-100 p-4 rounded-lg text-2xl font-mono flex justify-between items-center h-20 overflow-x-auto">
       <span>{{ display }}</span>
-      <button
-        @click="press('DEL')"
-        class="ml-2 p-1 rounded hover:bg-pink-300"
+     <button
+      @click="press('DEL')"
+      class="ml-2 p-1 rounded hover:bg-pink-300"
       >
-      <button @click="press('DEL')">
-        <Delete class="w-5 h-5 text-pink-600 " />
-      </button>
-
-      </button>
+      <Delete class="w-5 h-5 text-pink-600 " />
+     </button>
     </div>
 
-    <!-- Buttons Grid -->
     <div class="grid grid-cols-4 gap-3">
       <button
         v-for="btn in buttons"
